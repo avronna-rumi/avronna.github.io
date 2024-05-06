266bde251e56940d486e31c20417918bd6ee8dff
rumi naik <rumi@avronna.com>
2024-05-06 23:59:53 +0000

    try again

---
+++ templates/__react_static_root__/src/pages/index.bf442b46.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{56:function(e,t,n){"use strict";n.r(t);var c=n(0),r=n.n(c);t.default=function(){return r.a.createElement("div",{style:{textAlign:"center"}},r.a.createElement("h1",null,"Welcome to Our Underground-React-Static"))}}}]);
